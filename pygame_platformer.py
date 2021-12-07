d32ea1b376970d46a05830c7df49def345296c54
David Gilpin <dgilpin@bmchs.org>
2021-12-07 09:21:32 -0600

    started tutorial over

--- pygame_platformer.py
+++ pygame_platformer.py
@@ -3,19 +3,42 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
+vec = pygame.math.Vector2 # 2 for two dimensional
+
 
 # colors
 WHITE = (255, 255, 255)
 
 # variables
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 400
-BACKGROUND = pygame.image.load("background.png")
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 450
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
+
+CLOCK = pygame.time.Clock()
+
 
 # setup display
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-DISPLAYSURF.fill(WHITE)
-DISPLAYSURF.blit(BACKGROUND, (0,0))
+pygame.display.set_caption("Platformer Game")
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((128, 255, 40))
+        self.rect = self.surf.get_rect(center = (10, 420))
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((SCREEN_WIDTH, 20))
+        self.surf.fill((255, 0, 0))
+        self.rect = self.surf.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT -10))
+
+PT1 = Platform()
+P1 = Player()
 
 # Loop
 while True:
